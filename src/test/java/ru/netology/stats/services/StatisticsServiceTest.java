2588fd0b90d4e9e520be868913e888ed0cd8f083
Regina <OsRigli87@mail.ru>
2024-02-19 17:47:01 +0500

    full coverage

--- src/test/java/ru/netology/stats/services/StatisticsServiceTest.java
+++ src/test/java/ru/netology/stats/services/StatisticsServiceTest.java
@@ -16,5 +16,18 @@ public class StatisticsServiceTest {
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
+        System.out.println(actual);
+    }
+    @Test
+    void findMaxModified() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {0, 5, 8, 4, 5,3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+        System.out.println(actual);
     }
 }
