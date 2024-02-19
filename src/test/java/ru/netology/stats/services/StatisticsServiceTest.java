6ea83a53342593bd4c381d73a3d15c48f9c01c83
Regina <OsRigli87@mail.ru>
2024-02-19 20:21:53 +0500

    JaCoCo correction

--- src/test/java/ru/netology/stats/services/StatisticsServiceTest.java
+++ src/test/java/ru/netology/stats/services/StatisticsServiceTest.java
@@ -18,16 +18,4 @@ public class StatisticsServiceTest {
         Assertions.assertEquals(expected, actual);
         System.out.println(actual);
     }
-    @Test
-    void findMaxModified() {
-        StatisticsService service = new StatisticsService();
-
-        long[] incomesInBillions = {0, 5, 8, 4, 5,3, 8, 6, 11, 11, 12};
-        long expected = 12;
-
-        long actual = service.findMax(incomesInBillions);
-
-        Assertions.assertEquals(expected, actual);
-        System.out.println(actual);
-    }
 }
